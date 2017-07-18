4e0d0c4511916edd2179bd9bcc3ce021ea8249b5
louiskane <lkane02@yahoo.com>
2017-07-17 23:50:54 -0700

    adding file components

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//create a new component to produce html
+
+//take generated html and display
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function () {
+	return <div>HI!</div>;
+
+}
+
+ReactDOM.render(<App />, );
