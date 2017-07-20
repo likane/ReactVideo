ffa2f9b9ca9dfc7fe95495e2d6b5ff12d0fa6e56
louiskane <lkane02@yahoo.com>
2017-07-19 23:35:02 -0700

    video list

--- src/index.js
+++ src/index.js
@@ -2,26 +2,46 @@
 
 //take generated html and display
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = "AIzaSyCjvtL8Og-6jmUz4_DHroBIFDUoNcWz_PY";
 
-const App =  () => {
-	return ( 
 
-		<div>
-			
-			<SearchBar />
 
-		</div>
-	);
+class App extends Component  {
 
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surboards'}, (videos) => {
+			this.setState({videos});
+
+			//this.setState({videos: videos});
+		});
+	}
+
+	render() {
+
+		return ( 
+
+			<div>
+				
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+
+			</div>
+		);
+
+	}
+	
 
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
 
-//last tut: 15
+//last tut: 23
