2784de436aac36ba0bbe15ea736d3673e5774e9a
louiskane <lkane02@yahoo.com>
2017-07-20 23:32:10 -0700

    video list items

---
+++ src/components/video_list_item.js
@@ -0,0 +1,11 @@
+import REact from 'react';
+
+const VideoListItem = (props) => {
+	props.videos.map(())
+	
+	return (
+		<ul className="col-md-4 list-group"></ul>
+		);
+}
+
+export default VideoListItem;
