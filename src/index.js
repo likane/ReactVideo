4045133e155f2c1c74897d75209f53a32d2e6c4c
louiskane <lkane02@yahoo.com>
2017-07-23 12:53:44 -0700

    display selected video

--- src/index.js
+++ src/index.js
@@ -17,10 +17,16 @@ class App extends Component  {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+		};
 
 		YTSearch({key: API_KEY, term: 'surboards'}, (videos) => {
-			this.setState({videos});
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 
 			//this.setState({videos: videos});
 		});
@@ -33,7 +39,7 @@ class App extends Component  {
 			<div>
 				
 				<SearchBar />
-				<VideoDetail  video={this.state.videos[0]} />
+				<VideoDetail  video={this.state.selectedVideo} />
 				<VideoList videos={this.state.videos} />
 
 			</div>
